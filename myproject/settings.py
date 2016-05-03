b83dfba48908182dba0188a6407d3d9889ac4797
prob1995 <prob1995@gmail.com>
2016-05-03 17:14:19 +0800

    try to merge ssmm to on heroku 2

--- myproject/settings.py
+++ myproject/settings.py
@@ -364,7 +364,7 @@ STATICFILES_DIRS = (
 )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 #FOR HEROKU DEPLOY
