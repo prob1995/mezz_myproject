72680470e0cc9e57361914b1bb52b9e626187e18
prob1995 <prob1995@gmail.com>
2016-05-03 17:21:15 +0800

    try to merge ssmm to on heroku 3

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -25,3 +25,11 @@ from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+
+#FOR HEROKU DEPLOY
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
