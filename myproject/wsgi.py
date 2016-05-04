7729018d2efe531501eb90fee0e71210d33c3a6a
prob1995 <prob1995@gmail.com>
2016-05-04 14:41:40 +0800

    try

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -24,4 +24,12 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
+
+
+#FOR HEROKU DEPLOY
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
