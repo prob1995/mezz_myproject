b22dc3066aa4ef94c61581230ab55ec938b6da82
prob1995 <prob1995@gmail.com>
2016-05-07 21:45:00 +0800

    9

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -17,11 +17,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
 
 application = get_wsgi_application()
 
+'''
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+'''
 
 '''
 #Heroku suggestion  https://devcenter.heroku.com/articles/django-app-configuration
